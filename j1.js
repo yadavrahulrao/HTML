cf810e435c34d8e086e51b68c05fa67f945d2490
yadavrahulrao <rahul507538@gmail.com>
2025-12-26 21:31:31 +0530

    j1

--- j1.js
+++ j1.js
@@ -1,3 +1,114 @@
-console.log(" ther eis the change rahul");
+// name = "rahul"
+// age = 21
+// price = 234
 
-console.log("hello world");
+// radius = 14
+// x = null;
+// y = undefined;
+// console.log(name);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+
+// console.log(y);
+
+// console.log(radius);
+
+// isFollow = false;
+// console.log(isFollow);
+
+
+// name = "rahul"
+// Name = "rajbir"
+
+// console.log(name);
+
+// name = "rahul"
+// _name = "rahul"
+// $name = "rahul"
+
+// console.log(name);
+
+
+// fixed words cannot taken as variables
+
+// console = "rahul"
+
+// console.log(console)
+
+// let name = "rahul";
+// let price = 330;
+// let age = 24;
+// console.log(name);
+
+
+// var create a problem redeclare
+
+// var name = "rahul";
+
+// var name = "rajbir";
+
+// var name = "jony";
+// console.log(name);
+
+// const value can't change and through error 
+// const age = 24;
+// age = 34;
+// age = 45;
+// console.log(age);
+
+// if not given a value the show undefined
+// let a ;
+// console.log(a);
+
+
+// block 
+// {
+//   let a = 5;
+//   console.log(a);
+// }
+
+// {
+//   let a = 20;
+//   console.log(a);
+// }
+
+// type is number 
+// let age = 24;
+
+// let name = "rahul";
+
+// let isFollow = true;
+
+// let  a ;
+
+
+// let a = null ;
+
+// let x = BigInt("123");
+
+// let x = Symbol("hello");
+
+// const student = {
+//   name : "rahul",
+//   age : 20,
+//   cgpa : 7.5,
+//   isPass : true,
+// }
+// student ["cgpa"] = student["cgpa"] + 1;
+// student["age"] = student["age"] + 1;
+// console.log(student.age);
+
+// console.log(student["cgpa"]);
+
+
+
+// question 1 for a pen flikart
+// const product = {
+//   name : "Parker Jotter CT Ball Pen (Black)",
+//   rating : 4   ,
+//   deal : "Deal of the Day",
+//   price : 270,
+//   offer :"5% off",
+
+// }

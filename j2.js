fa1040a6412edf7ecefcce4d9222dcd17b1a87ff
yadavrahulrao <rahul507538@gmail.com>
2025-12-28 21:25:34 +0530

    java

---
+++ j2.js
@@ -0,0 +1,187 @@
+// comments : the part of code that is not executed 
+
+/*this is a multiline comment should 
+   be place */
+
+// console.log("hello rahul !")
+
+
+// arithmetic operators
+
+// let a = 5;
+// let b = 5;
+// console.log("a+b:",a+b);
+// console.log("a-b:",a-b);
+
+// console.log("a*b:",a*b);
+// console.log("a/b :",a/b);
+
+// console.log("a mod b:",a%b);
+// console.log("a expo b:",a**b);
+
+// a++;
+// console.log("a inc :",a);
+
+// b--;
+// console.log("b dec:",b);
+
+
+
+
+
+// let a = 5;
+
+// console.log(a++);
+// console.log(a);
+
+// console.log(--a);
+
+// assignment operator
+
+// let a = 3;
+// let b = 5;
+
+// a += 4;
+
+// b -= 2;
+
+// console.log(a);
+// console.log(b);
+
+// a *= 3;
+// console.log(a);
+
+// b %= 5;
+// console.log(b);
+
+// a **= 4;
+// console.log(a);
+
+// comparision operator
+
+// let a = 4;
+// let b = 5;
+
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<b);
+// console.log(a<=b);
+
+
+// logical operators
+
+// let a = 5;
+// let b = 5;
+
+// let c = !(a>b);
+// let c1= a==b;
+
+// console.log(c&&c1);
+// console.log(c||c1);
+// console.log(c&&c1);
+
+
+// conditional statements
+
+// let age = 25;
+
+// if(age >= 18){
+//   console.log("you can vote");
+// }
+
+// if(age < 18){
+//   console.log("you cant vote");
+// }
+
+// let mode = "dark";
+// let color;
+
+// if(mode == "dark"){
+//   color = "black";
+// }
+// if(mode == "light"){
+//   color = "white";
+// }
+
+// console.log(color);
+
+// let age = 15;
+// if(age>= 18){
+//   console.log("you can vote");
+// }
+// else{
+//   console.log("you cant vote");
+// }
+
+
+// let num = 10 ;
+// if (num % 2 === 0){
+//   console.log("even");
+
+// }
+// else{
+//   console.log("odd");
+// }
+
+
+// let age = 21;
+// if(age < 18){
+//   console.log("child");
+
+// }
+// else if(age > 18 && age <60){
+//   console.log("adult");
+// }
+// else {
+//   console.log("senior");
+// }
+
+
+
+// ternary operator
+
+// let age = 25;
+// let a = age>=81 ? "adult" : "not adult";
+
+// console.log(a);
+
+// alert("hello"); one time hello
+
+
+// take a input
+// let name = prompt("hello");
+// console.log(name);
+
+// let num = prompt("Eneter a number:");
+
+// if(num % 5 === 0){
+//   console.log("true");
+// }
+// else{
+//   console.log("false");
+// }
+
+// let mark = prompt("enter marks:");
+
+// if(mark<=100 && mark >=90){
+//   console.log("A");
+// }
+// else if(mark<=89 && mark>=70){
+//   console.log("B");
+// }
+// else if(mark <=69 && mark >= 60){
+//   console.log("C");
+// }
+// else if(mark <=59 && mark >=50){
+//   console.log("D");
+// }
+// else {
+//   console.log("F");
+// }
+
+
---
+++ j3.js
@@ -0,0 +1,6 @@
+// loop : to execute a piece of code again and again
+
+let a = 4;
+for(let i = 1;i<=a ;i++){
+  console.log("rahul");
+}

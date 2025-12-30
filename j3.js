50d17ea60ddf0e84e9391311ea45e76e15aee706
yadavrahulrao <rahul507538@gmail.com>
2025-12-30 21:15:04 +0530

    js

--- j3.js
+++ j3.js
@@ -1,6 +1,38 @@
 // loop : to execute a piece of code again and again
 
-let a = 4;
-for(let i = 1;i<=a ;i++){
-  console.log("rahul");
-}
+// let a = 4;
+// for(let i = 1;i<=a ;i++){
+//   console.log("rahul");
+// }
+
+// console.log("loop has ended");
+
+
+// sum of n numbers
+// let num = prompt("enter a number");
+// let sum = 0;
+// for (let i = 1; i<= num ; i++){
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// for(let i = 0 ; i <=5 ; i++){
+//   console.log(i);
+// }
+
+
+// while loop 
+// let i = 0;
+// while( i <= 5){
+//   console.log("rahul");
+//   i++;
+// }
+
+// let i = 20;
+// do{
+//   console.log("rahul");
+//   i++;
+// }
+// while(i<= 5);
+
+

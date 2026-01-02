f6b9c301b3c25c21f58a10e5171cfcfb76ceefde
yadavrahulrao <rahul507538@gmail.com>
2026-01-02 21:54:49 +0530

    loop

--- j3.js
+++ j3.js
@@ -36,3 +36,105 @@
 // while(i<= 5);
 
 
+// let s = "rahul";
+// for (let i of s){
+//   console.log(i);
+// }
+
+
+// let student = {
+//   name :"rahul",
+//   age :21 ,
+//   cgpa : 7.5,
+//   isPass : true,
+// };
+// for (let key in student ){
+//   console.log("key:",key , "value :",student[key]);
+// }
+
+// for(let i = 0 ; i <= 100 ; i++){
+//   if(i%2===0){
+//     console.log(i);
+  
+//   }
+// }
+
+
+// game 
+
+
+// let gameNum = 25;
+
+// let userNum = prompt("guess the game number");
+
+// while(userNum != gameNum){
+//   userNum = prompt("this guess is wrong , try again")
+
+// }
+// console.log("cangtrats finally you guess !");
+
+
+
+// let str = "rahul";
+
+// let stre = 'rahul';
+
+// console.log(str.length);
+
+// console.log(str[0]);
+
+
+// let sentence = `this is a templete literal`;
+// console.log(sentence);
+
+
+// let obj = {
+//   item : "pen",
+//   price : 55,
+// };
+
+// let o = `the cost of ${obj.item} is ${obj.price}`;
+// console.log(o);
+
+// console.log("cost of",obj.item , "is",obj.price);
+
+
+// let o = `this is a template literal ${1+2+3}`;
+// console.log(o);
+
+
+// console.log("rahul\nyadav");
+
+// console.log("rahul\tyadav");
+
+// let str = "rahul";
+// console.log(str);
+
+// let o = "YADAV";
+// console.log(o.toLowerCase());
+
+
+// let s = "      rahul        ";
+// console.log(s.trim());
+
+
+// let a = "hello rahul";
+// console.log(a.slice(1,3));
+
+
+// let a = "rahul";
+// let b = "yadav";
+// console.log(a.concat(b));
+
+// let a = "rahul";
+// console.log(a.replace("ah",123));
+
+// let s = "rahul";
+// console.log(s.charAt(4));
+
+
+// let username = prompt("enter username:");
+// let userlength = username.length;
+// let s = "@"+username+userlength;
+// console.log(s);
+

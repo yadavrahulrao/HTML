86870988d208fb252014a4652657549577523214
yadavrahulrao <rahul507538@gmail.com>
2026-01-24 22:05:47 +0530

    j5

--- j5.js
+++ j5.js
@@ -1,8 +1,92 @@
 // functions
-function hello(){
-  console.log("hello rahul");
-  console.log("this is a function");
+// function hello(){
+//   console.log("hello rahul");
+//   console.log("this is a function");
 
-}
+// }
 
-hello();
+// hello();
+// hello();
+
+
+
+// function myfunction(msg){
+  
+// }
+
+// console.log("hello");
+// "abc".toUpperCase()
+// "xyz".toUpperCase
+
+// [1,2,3].push(4);
+
+
+
+// function myfunction(msg,n){
+//   console.log(msg*n);
+// }
+// myfunction("hello",100);
+
+
+
+// function sum(x,y){
+//   console.log(x+y);
+// }
+
+
+// function sum(x,y){
+//   s = x+y;
+//   console.log("before");
+//     return s;
+//   console.log("after");
+// }
+// let value = sum(3,4);
+// console.log(value);
+
+
+// function sum(a,b){
+//   return a+b;
+// }
+
+// const arrowsum = (a,b) =>{
+//   console.log(a+b);
+// };
+
+
+
+// function mul(a,b){
+//   return a*b;
+
+// }
+
+// const arroemul = (a,b) => {
+//   console.log(a*b);
+// };
+
+
+// const printhello = () =>{
+//   console("hello");
+// };
+
+
+// function count_vowels(str){
+//     let count = 0;
+//   for(const char of str){
+//     if(char === "a"|| char === "e"|| char === "i" || char === "o"  || char === "u"){
+//       count ++;
+//     }
+//   }
+//   console.log(count);
+// }
+
+// const count_vow = (str) => {
+//     let count = 0;
+//   for(const char of str){
+//     if(char === "a"|| char === "e"|| char === "i" || char === "o"  || char === "u"){
+//       count ++;
+//     }
+//   }
+//   console.log(count);
+// }
+
+

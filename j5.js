54f5b885b04074f572a06edf11dbd9ccbe960e6a
yadavrahulrao <rahul507538@gmail.com>
2026-01-25 18:56:03 +0530

    functions

--- j5.js
+++ j5.js
@@ -90,3 +90,105 @@
 // }
 
 
+// function abc(){
+//   console.log("hello");
+// }
+
+// function my(abc){
+// return abc;
+// }
+// abc();
+
+
+// let arr = [1,2,3,4,5];
+// arr.forEach((val)=>{
+//   console.log(val);
+// });
+
+
+// let arr = [1,2,3,4,5,6];
+// arr.forEach((val,idx,arr)=>{
+//   console.log(val,idx,arr);
+// });
+
+
+// square of each no. in a array
+// let arr = [1,2,3,4,5];
+// arr.forEach((val)=>{
+//   console.log(val*val);
+// });
+
+
+// map 
+// let arr = [1,2,3,4];
+// arr.map((val)=>{
+//   console.log(val);
+// });
+
+
+// let arr = [1,2,3,4,5];
+// let newArr = arr.map((val)=>{
+//   return val;
+// });
+
+// console.log(newArr);
+
+
+
+
+// filter method
+// let arr = [1,2,3,4,5,6];
+
+// let newArr = arr.filter((val)=>{
+//   return val % 2 === 0;
+// });
+
+// console.log(newArr);
+
+// reduce
+// let arr = [1,2,3,4];
+
+// let i = 0;
+// let sum = arr.reduce((accu,val)=>accu + val ,i ,);
+// console.log(sum);
+
+// let newARR = arr.reduce((res,val)=>{
+//   return res+val ;
+// });
+
+// console.log(newARR);
+
+
+// let arr = [1,234,4,55,,6,4];
+// let newArr = arr.reduce((res,val)=>{
+//   return res>val ? res : val ;
+// });
+// console.log(newArr);
+
+
+
+// let marks = [12,78,93,56,78,90];
+// let topper = marks.filter((val)=>{
+//   return val > 90 ;
+// });
+// console.log(topper);
+
+
+let n = prompt("enter a number :");
+let arr = [];
+for (let i = 1 ; i <= n ; i++){
+  arr[i-1] = i;
+}
+console.log(arr);
+
+let sum = arr.reduce((res,val)=>{
+  return res+val;
+
+});
+
+console.log(sum);
+
+let product = arr.reduce((res,val)=>{
+  return res*val;
+});
+console.log(product);

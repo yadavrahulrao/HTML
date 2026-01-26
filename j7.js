f99b7a1c07141fb4fa334fd3b26d0efe7d655f1e
yadavrahulrao <rahul507538@gmail.com>
2026-01-26 17:30:53 +0530

    dom

---
+++ j7.js
@@ -0,0 +1,30 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let par = document.querySelector("p");
+// console.log(par.setAttribute("class","new class"));
+
+// let div = document.querySelector("div");
+// console.dir(div);
+// div.style.backgroundColor = "green";
+
+// div.style.fontSize = "28px";
+
+// div.innerText = "hello";
+
+// let newHead = document.createElement("h1");
+// newHead.innerHTML = "this sis a a new ";
+// document.querySelector("body").prepend(newHead);
+
+
+// let button = document.createElement("button");
+// button.innerText = "click me";
+// button.style.color = "black";
+// button.style.backgroundColor = "green";
+// document.querySelector("body").prepend(button);
+
+let par = document.querySelector("p");
+par.setAttribute("class","rao");
